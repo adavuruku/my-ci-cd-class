a570af55c651c2ed16c09df2676b776be1340430
Abdulraheem Sherif Adavuruku <aabdulraheemsherif@gmail.com>
2024-06-13 01:57:03 +0100

    update test

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -21,3 +21,32 @@ describe('New Endpoint', () => {
     expect(res.statusCode).toEqual(201)
   })
 })
+
+describe('Get a user', () => {
+  it('should return a static json response of user object with status 200', async () => {
+    const res = await request(app)
+      .get('/users/1')
+    expect(res.body).toEqual({
+      id: 1,
+      firstName: 'Sherif',
+      lastName: 'Adams',
+      age: 25
+    })
+    expect(res.statusCode).toEqual(200)
+  })
+})
+
+describe('Get all users', () => {
+  it('should return a static json response of user object list with status 200', async () => {
+    const res = await request(app)
+      .get('/users')
+    expect(res.body[1]).toEqual({
+      id: 2,
+      firstName: 'Joy',
+      lastName: 'Amoka',
+      age: 18
+    })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body.length).toEqual(3)
+  })
+})
